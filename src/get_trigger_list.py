818fe8ddb3a83ae84521a72c22f8261ce5087077
mdafcikova <marianna.dafcikova@gmail.com>
2022-11-16 23:38:39 +0100

    modified for arkham & removed weak INTEGRAL triggers

--- src/get_trigger_list.py
+++ src/get_trigger_list.py
@@ -5,7 +5,7 @@ Creates a list of GRB triggers in the past x hours.
 import pandas as pd
 from pyorbital.orbital import Orbital
 
-trigger_list = pd.read_csv(r'C:\Users\maria\Desktop\CubeSats\all_triggers.csv')
+trigger_list = pd.read_csv('/home/ripa/grbalpha/all_triggers.csv')
 
 timestamp = trigger_list.grb_date.astype('datetime64')
 end_time = pd.to_datetime('now') - pd.Timedelta('58 h')
@@ -16,12 +16,15 @@ url = []
 
 i = 0
 while (timestamp[i] > pd.Timestamp(end_time)):
-    utc.append(trigger_list.grb_date[i])
-    mission.append(trigger_list.mission[i])
-    url.append(trigger_list.url[i])
+    if (trigger_list.mission[i] == 'INTEGRAL ISGRI GCN' & trigger_list.note[i] == 'Weak'):
+        continue
+    else:
+        utc.append(trigger_list.grb_date[i])
+        mission.append(trigger_list.mission[i])
+        url.append(trigger_list.url[i])
     i += 1
 
-orb = Orbital('GRBAlpha')
+orb = Orbital('GRBAlpha', tle_file='/home/mdafcikova/tle.txt')
 lon, lat, alt = orb.get_lonlatalt(pd.Series(utc))
 
 def day_fraction(hh,mm,ss):
@@ -29,15 +32,12 @@ def day_fraction(hh,mm,ss):
 
 dfrac = [round(day_fraction(int(t[11:13]),int(t[14:16]),int(t[17:19])),5) for t in utc ]
 
-file_frac = []
 F = []
 
 for frac in dfrac:
     if 2*frac > 1:
-        # file_frac.append(round(2*frac - 1,5))
         F.append(round(2*frac - 1,3))
     else:
-        # file_frac.append(round(2*frac,5))
         F.append(round(2*frac,3))
 
 df = pd.DataFrame(columns=['UTC','F','day_fraction','f','longitude','latitude','mission','URL'])
@@ -51,4 +51,5 @@ df.latitude = lat.round(2)
 df.mission = mission
 df.URL = url
 
-df.to_csv('daily_trigger_list.csv',index=False)
+# df.to_csv('daily_trigger_list.csv',index=False)
+print(df)
