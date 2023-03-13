9e8f2efc965bca9379efe075bc6f5e8e070e20e3
Marianna Dafcikova <marianna.dafcikova@gmail.com>
2023-03-13 16:12:28 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='grbalpha-tools',
+    packages=find_packages(),
+    version='0.1.0',
+    description='tools to work with data from GRBAlpha and VZLUSAT-2 CubeSats',
+    author='Marianna Dafcikova',
+)
