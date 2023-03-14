af5893bd72e5641d35df2dcf46b14ecfe14a55c9
Marianna Dafcikova <marianna.dafcikova@gmail.com>
2023-03-14 19:03:37 +0100

    installation + requirements

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This is a package designed to visualize and do basic analysis of data from the GRBAlpha and VZLUSAT-2 CubeSats. Few examples of its usage are described in the Jupiter notebook _grbalpha-tools-intro.ipynb_.
 
+## Instalation
+
+You can install *GRBAlpha tools* via [pip]:
+
+```console
+pip install git+https://github.com/mdafcikova/grbalpha-tools.git
+```
--- setup.py
+++ setup.py
@@ -5,4 +5,14 @@ setup(
     version='0.1.0',
     description='tools to work with data from GRBAlpha and VZLUSAT-2 CubeSats',
     author='Marianna Dafcikova',
+    install_requires=[         
+        'dataclasses>=0.6',         
+        'pandas>=1.5.1',
+        'numpy>=1.23.4',
+        'matplotlib>=3.6.2',
+        'astropy>=5.1.1',
+        'scipy>=1.9.3',
+        'os',
+        'warnings',
+    ],
 )
