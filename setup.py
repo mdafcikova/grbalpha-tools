59e5f512f9f3f82911c180dbd7800ee4c56da27e
Marianna Dafcikova <marianna.dafcikova@gmail.com>
2023-10-09 20:13:41 +0200

    updated install_requires

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ from setuptools import find_packages, setup
 setup(
     name='grbalpha-tools',
     packages=find_packages(),
-    version='0.1.7',
+    version='0.1.8',
     description='tools to work with data from GRBAlpha and VZLUSAT-2 CubeSats',
     author='Marianna Dafcikova',
     install_requires=[         
         'astropy>=5.1.1',
         'pyorbital>=1.7.3',
+        'pandas',
+        'dataclasses',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'os',
+        'warnings'
     ],
 )
