80d703ded8ef90cb027a7bcfb4cbe711c392115c
Marianna Dafcikova <marianna.dafcikova@gmail.com>
2024-03-08 16:49:50 +0100

    returned previous commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='grbalpha-tools',
     packages=find_packages(),
-    version='0.1.13',
+    version='0.1.14',
     description='tools to work with data from GRBAlpha and VZLUSAT-2 CubeSats',
     author='Marianna Dafcikova',
     install_requires=[         
